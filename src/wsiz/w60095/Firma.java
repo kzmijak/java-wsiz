e833c1371d5897dcdf64f24379012881c7e42655
kzmijak <kzmijak@gmail.com>
2020-06-14 12:36:31 +0200

    Lab 5
    Do stworzonego przez siebie projektu stwórz:
    -klasę Firma, w której będzie tworzona Mapa pracowników firmy

---
+++ src/wsiz/w60095/Firma.java
@@ -0,0 +1,43 @@
+package wsiz.w60095;
+
+import java.util.HashMap;
+
+public class Firma {
+    private HashMap<Integer, Pracownik> pracownicy;
+
+    public Firma()
+    {
+        this.pracownicy = new HashMap<Integer, Pracownik>();
+    }
+
+    public Firma(HashMap<Integer, Pracownik> pracownicy) {
+        this.pracownicy = pracownicy;
+    }
+
+    public HashMap<Integer, Pracownik> getPracownicy() {
+        return pracownicy;
+    }
+
+    public void DodajPracownika(Pracownik nowyPracownik)
+    {
+        if(!this.pracownicy.values().contains(nowyPracownik))
+        {
+            int pierwszeWolneId =
+                    this.pracownicy
+                            .keySet()
+                            .stream()
+                            .filter(id -> this.pracownicy.get(id+1) == null)
+                            .toArray(Integer[]::new)
+                            [0];
+            this.pracownicy.put(pierwszeWolneId, nowyPracownik);
+        }
+    }
+
+    public void UsunPracownika(Pracownik pracownikDoUsuniecia)
+    {
+        if(this.pracownicy.values().contains(pracownikDoUsuniecia))
+        {
+            this.pracownicy.values().remove(pracownikDoUsuniecia);
+        }
+    }
+}
